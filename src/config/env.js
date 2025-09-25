cde994227dafad6a5d52a03f12e48d5d39e6efab
starhq <50849806@qq.com>
2025-09-25 11:26:29 +0800

    backend done

--- src/config/env.js
+++ src/config/env.js
@@ -4,4 +4,5 @@ export const ENV = {
   PORT: process.env.PORT || 5001,
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
+  API_URL: process.env.API_URL || "http://localhost:5001/api/health",
 };
